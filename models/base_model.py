4de54d24a5aeef7b951a8d6e8d7d2c153743b0f1
0x41mujt4b4 <almujtaba007@gmail.com>
2024-03-23 10:08:56 -0700

    add AirBnB_clone

---
+++ models/base_model.py
@@ -0,0 +1,17 @@
+from uuid import uuid4 as uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid())
+        self.created_at = datetime.today().isoformat()
+        self.updated_at = datetime.today().isoformat()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
+
+    def save(self):
+        self.updated_at = datetime.today().isoformat()
+    
+    def to_dict(self):
+        return {**self.__dict__, "__class__": self.__class__.__name__}
